a60dfc6ee7855de9f59cc2bb46d30adeb25fc581
FranciscoLlamas <francisco190696@gmail.com>
2023-10-04 12:07:43 -0600

    react

---
+++ src/Components/About.jsx
@@ -0,0 +1,29 @@
+import './About.css';
+
+
+
+const About = () => {
+     return (
+            <div className='cardName'>
+                <h1 className='Nombre'>Francisco Llamas</h1>
+                <h2>27 años </h2>
+                <p>correo frank@gmail.com</p>
+                <br></br>
+
+
+                <p>Soy Francisco Llamas estudie Ingenieria en Transporte y por 
+                   
+                </p>
+                <p> el momento estoy ttranscicionando en el mundo de la programacion
+                    </p>
+                <p>me encanta aprender y seguir mejorando cada dia
+                    
+                </p>
+
+            </div>
+     )
+
+     }
+
+
+export default About;
